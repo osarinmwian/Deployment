5ef23366cc172cf19188a6e0b43c5172d31a77a3
osarinmwian <noel.osarinmwian@gmail.com>
2023-03-10 23:47:58 +0100

    remove local properties

--- App.tsx
+++ App.tsx
@@ -80,6 +80,10 @@ function App(): JSX.Element {
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+          </Section>
+
           <Section title="See Your Changes">
             <ReloadInstructions />
           </Section>
